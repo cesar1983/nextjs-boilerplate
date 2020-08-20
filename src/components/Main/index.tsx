317897f3a7884631270d9653002f4f69b06143c3
Cesar Fernandes <cesar.fa@gmail.com>
2020-08-19 23:46:44 -0300

    config styled component e setup main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,15 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h2>Trad</h2>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Logotipo da Trad" />
+    <S.Title>Trad</S.Title>
+    <S.Description>Controle suas operações</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Ilustração de um desenvolvedor de frente para a tela do computador"
+    />
+  </S.Wrapper>
 )
 
 export default Main
