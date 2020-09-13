7635613de658b137c1561758b75a2cfad88b2ad4
Cesar Fernandes <cesar.fa@gmail.com>
2020-09-13 15:10:58 -0300

    ajuste config webpack storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,20 @@
-import GlobalStyles from  '../src/styles/global'
+//import GlobalStyles from '../src/styles/global'
+/**
+ * Por conta de uma configuracao feita no webpack do
+ * storybook, podemos incluir tudo a partir da pasta src,
+ * ao invés de fazer como as duas linhas de cima
+ */
+import GlobalStyles from 'styles/global'
 
 export const decorators = [
   (Story) => (
-      <>
-        <GlobalStyles />
-        <Story />
-      </>
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
   )
 ]
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' }
 }
